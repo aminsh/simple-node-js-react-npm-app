c1817076d3abfb99c424535935c390ec1e7ed19e
Amin <aminsheikhi@gmail.com>
2018-07-10 20:22:12 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log('n');
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
